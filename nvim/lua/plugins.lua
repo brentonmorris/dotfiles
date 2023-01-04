88250b3b3bcded9744b245ccd5449a52a4d8d138
Brenton Morris <brentonamorris@gmail.com>
2023-01-04 11:42:36 -0500

    Add some plugins, take some plugins

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -7,29 +7,26 @@ end
 
 packer.startup(function(use)
   use 'wbthomason/packer.nvim'
-  use {
-    'svrana/neosolarized.nvim',
-    requires = { 'tjdevries/colorbuddy.nvim' }
-  }
 
   use 'L3MON4D3/LuaSnip'
   use 'RRethy/nvim-treesitter-endwise'
-  use 'akinsho/nvim-bufferline.lua' -- tabs
-  use 'dinhhuy258/git.nvim' -- For git blame & browse
-  use 'glepnir/lspsaga.nvim' -- LSP UIs
-  use 'hrsh7th/cmp-buffer' -- nvim-cmp source for buffer words
-  use 'hrsh7th/cmp-nvim-lsp' -- nvim-cmp source for neovim's built-in LSP
-  use 'hrsh7th/nvim-cmp' -- Completion
-  use 'junegunn/seoul256.vim' -- colorscheme
-  use 'kyazdani42/nvim-web-devicons' -- File icons
+  use 'akinsho/nvim-bufferline.lua'
+  use 'glepnir/lspsaga.nvim'
+  use 'hrsh7th/cmp-buffer'
+  use 'hrsh7th/cmp-nvim-lsp'
+  use 'hrsh7th/nvim-cmp'
+  use 'junegunn/seoul256.vim'
+  use 'kyazdani42/nvim-web-devicons'
   use 'lewis6991/gitsigns.nvim'
-  use 'neovim/nvim-lspconfig' -- Configurations for Nvim LSP
+  use 'neovim/nvim-lspconfig'
   use 'norcalli/nvim-colorizer.lua'
-  use 'nvim-lua/plenary.nvim' -- Common utilities
-  use 'nvim-lualine/lualine.nvim' -- Statusline
+  use 'nvim-lua/plenary.nvim'
+  use 'nvim-lualine/lualine.nvim'
   use 'nvim-telescope/telescope-file-browser.nvim'
   use 'nvim-telescope/telescope.nvim'
-  use 'onsails/lspkind-nvim' -- vscode-like pictograms
+  use 'onsails/lspkind-nvim'
+  use 'tpope/vim-fugitive'
+  use 'tpope/vim-surround'
   use 'williamboman/mason-lspconfig.nvim'
   use 'williamboman/mason.nvim'
   use 'windwp/nvim-autopairs'
