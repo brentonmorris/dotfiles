b24bf21b9b670516d10fb7dd7935167c4ac855b0
Brenton Morris <brentonamorris@gmail.com>
2023-01-05 13:40:03 -0500

    Turn off buffer wrapping

--- nvim/lua/base.lua
+++ nvim/lua/base.lua
@@ -23,7 +23,7 @@ vim.opt.shiftwidth = 2
 vim.opt.tabstop = 2
 vim.opt.ai = true -- Auto indent
 vim.opt.si = true -- Smart indent
-vim.opt.wrap = true
+vim.opt.wrap = false
 vim.opt.backspace = 'start,eol,indent'
 vim.opt.path:append { '**' } -- Finding files - search subfolders'
 vim.opt.wildignore:append { '*/node_modules/*' }
