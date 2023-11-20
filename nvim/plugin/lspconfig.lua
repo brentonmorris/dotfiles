150423ebfe372bd4505775479825f5a183530631
Brenton Morris <brentonamorris@gmail.com>
2023-11-20 17:16:31 -0500

    Make warning go away.

--- nvim/plugin/lspconfig.lua
+++ nvim/plugin/lspconfig.lua
@@ -21,7 +21,7 @@ end
 
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
-local on_attach = function(client, bufnr)
+local on_attach = function(_client, bufnr)
   local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
 
   --Enable completion triggered by <c-x><c-o>
