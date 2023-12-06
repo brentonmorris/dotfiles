c4b69084deadc74b095341590003fd1b90c1c4fe
Brenton Morris <brentonamorris@gmail.com>
2023-12-06 13:51:56 -0500

    Remove cmp_luasnip

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -24,7 +24,6 @@ require('lazy').setup({
   "hrsh7th/cmp-buffer",
   "hrsh7th/cmp-nvim-lsp",
   "github/copilot.vim",
-  "saadparwaiz1/cmp_luasnip",
   -- "hrsh7th/cmp-cmdline",
   -- "hrsh7th/cmp-path",
   "nvimdev/lspsaga.nvim",
